9643436b89f9bb76ac0f3da1d397a2f61393fe9b
sobhy <am24873@gmail.com>
2023-01-01 16:56:26 +0200

    search, add, list

--- Repository_System/Repository_System/Article/urls.py
+++ Repository_System/Repository_System/Article/urls.py
@@ -3,4 +3,6 @@ from .views import *
 urlpatterns = [
     path('', home),
     path('<int:id>',article_details),
+    path('create',article_create),
+    path('search',article_search)
 ]
--- Repository_System/Repository_System/Article/views.py
+++ Repository_System/Repository_System/Article/views.py
@@ -15,3 +15,34 @@ def article_details(request,id=None):
         'article': article_obj
     }
     return HttpResponse(render(request,'article.html',context))
+
+def article_create(request):
+    context = {
+        'title': None,
+        'description': None,
+        'status': None
+    }
+    if request.POST:
+        article_obj = Article(title=request.POST['title'],desc=request.POST['desc'])
+        context = {
+            'title': request.POST['title'],
+            'description': request.POST['desc'],
+            'status': 'created'
+        }
+        article_obj.save()
+    return render(request,'create.html',context)
+
+def article_search(request):
+    context = {
+        'article': None,
+    }
+    if request.GET:
+        try:
+            int_query = int(request.GET['q'])
+            article_obj = Article.objects.get(id=int_query)
+            context = {
+                'article': article_obj
+            }
+        except:
+            pass
+    return render(request,'search.html',context)
